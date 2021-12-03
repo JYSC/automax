071e17d8727ae23c35ac777dcfcc54a2d6d464c7
JYSC <a329575@uach.mx>
2021-12-02 21:02:07 -0700

    Merge branch 'main' of https://github.com/JYSC/automax

--- conexion.php
+++ conexion.php
@@ -2,19 +2,15 @@
 include_once('db.php');
 
 //recibe los datos del formulario de la tabla miembros
-$nombre_miembro=$_POST['nombre_miembro'];
-$apellido_miembro=$_POST['apellido_miembro'];
-$direccion_miembro=$_POST['direccion_miembro'];
-$codigo_miembro=$_POST['codigo_miembro'];
-$telefono_miembro=$_POST['telefono_miembro'];
-
-echo "los datos son los siguientes ";
-echo "$nombre_miembro, $apellido_miembro, $direccion_miembro, $codigo_miembro y $telefono_miembro";
-
+$nombre=$_POST['nombre'];
+$apellido=$_POST['apellido'];
+$email=$_POST['email'];
+$telefono=$_POST['telefono'];
+$personas=$_POST['personas'];
 
 $conectar=conn();
-$sql="INSERT INTO miembros(nombre_miembro, apellido_miembro, direccion_miembro, codigo_miembro,telefono_miembro)
-VALUES ('$nombre_miembro', '$apellido_miembro','$direccion_miembro','$codigo_miembro','$telefono_miembro')";
+$sql="INSERT INTO rentas(nombre, apellido, email,telefono, personas)
+VALUES ('$nombre', '$apellido','$email','$telefono' ,'$personas')";
 $resul = mysqli_query($conectar, $sql)or trigger_error("Query Failed! SQL Error:".mysqli_error($conectar), E_USER_ERROR);
 
 echo "$sql";
